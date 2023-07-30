14ec47a82c391b442427b772cacd51b2b5d366c8
Devika Shailesh Olkar <devikaolkar@kccemsr.edu.in>
2023-07-30 15:33:55 +0530

    pushed latest changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import "./App.css";
 import CreateBlog from "./components/pages/CreateBlog";
-import { Provider } from "react-redux";
-import blogStore from "./reduxfiles/blogStore";
 import {
   Route,
   createBrowserRouter,
@@ -16,8 +14,11 @@ import Help from "./components/pages/Help";
 import { BlogList } from "./components/pages/BlogList";
 import { SingleBlog } from "./components/pages/SingleBlog";
 import { blogsLoader } from "./components/pages/BlogList";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { fetchBlogs } from "./reduxfiles/blogActions";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,18 +28,23 @@ const router = createBrowserRouter(
       <Route path="about" element={<About />} />
       <Route path="help" element={<Help />} />
       <Route path="blog" element={<BlogList />} loader={blogsLoader} />
-      <Route path=":id" element={<SingleBlog />}/>
+      <Route path=":id" element={<SingleBlog />} />
       <Route path="*" element={<NotFound />} />
-    </Route>  
+    </Route>
   )
 );
 
 function App() {
-  return (
-    <Provider store={blogStore}>
-      <RouterProvider router={router} />
-    </Provider>
-  );
+  const { isBlogFetched, blogsList } = useSelector((state) => state);
+  const dispatch = useDispatch();
+  // the if conditions run only when the blog length is 0 i.e blogsList: [], once its get filled the useEffect wont run
+  useEffect(() => {
+    if (!isBlogFetched && blogsList.length == 0) {
+      dispatch(fetchBlogs());
+    }
+  }, []);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,19 +1,7 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchBlogs } from "./../../reduxfiles/blogActions";
+import React from "react";
 import { BlogList } from "./BlogList";
-import myStyle from "../../components/myStyle.css";
 
 function HomePage() {
-  const { isBlogFetched, blogsList } = useSelector((state) => state);
-  const dispatch = useDispatch();
-  // the if conditions run only when the blog length is 0 i.e blogsList: [], once its get filled the useEffect wont run
-  useEffect(() => {
-    if (!isBlogFetched && blogsList.length == 0) {
-      dispatch(fetchBlogs());
-    }
-  }, []);
-
   return (
     <div>
       <h1 className="homePageTitle">Blog Page</h1>
